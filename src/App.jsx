de9f4c30411ebe805ebc20077c12050ccdf133b1
smnahidweb <smnahidh040@gmail.com>
2025-07-07 22:24:56 +0600

    added basic layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h2 className='text-5xl text-center'>Hello This is Life Insurance Mangaemnt Systems</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Router/Router.jsx
@@ -0,0 +1,108 @@
+import { createBrowserRouter} from "react-router";
+import RootLayout from "../Layout/RootLayout";
+import Home from "../Pages/Home/Home";
+
+ export const router = createBrowserRouter([
+  {
+  path: "/",
+   Component:RootLayout,
+   children:[
+    {
+        index:true,
+        Component:Home,
+        
+    },
+    
+   ]
+  },
+//   auth layout route started
+//   {
+//     path:'/',
+//     Component:AuthLayout,
+//     children:[
+//       {
+//         path:'/login',
+//         Component:Login
+//       },{
+//         path:'/register',
+//         Component:Register
+//       }
+//     ]
+//   },
+// Dashboard layout is started
+//   {
+//     path:'/dashboard',
+//     element:<PrivateRoute>
+//       <Dashboard></Dashboard>
+//     </PrivateRoute>,
+//     children:[
+//       {
+//         index:true,
+//         Component:DashBoardHome
+//       },
+//       {
+//         path:'myParcels',
+//         Component:MyParcel
+//       },
+//       {
+//         path:'payment/:parcelId',
+//         Component:Payment
+//       },
+//       {
+//         path:'paymentHistory',
+//         Component:PaymentHistory
+//       },
+
+
+
+//       // riders routes
+//       {
+//         path:'pending-tasks',
+//         element:<RiderRoute>
+//           <PendingTask></PendingTask>
+//         </RiderRoute>
+
+//       },
+//       {
+//         path:'delivered-task',
+//         element:<RiderRoute>
+//           <Deliveredtask></Deliveredtask>
+//         </RiderRoute>
+//       },
+//       {
+//         path:'myEarning',
+//         element:<RiderRoute>
+//           <MyEarning></MyEarning>
+//         </RiderRoute>
+
+//       },
+
+
+//       {
+//         path:'pendingRiders',
+//         element: <AdminRoute>
+//           <PendingRiders></PendingRiders>
+//         </AdminRoute>
+//       }
+//       ,{
+//         path:'activeRiders',
+//        element:<AdminRoute>
+//         <ActiveRiders></ActiveRiders>
+//        </AdminRoute>
+
+//       },
+//       {
+//         path:"makeAdmin",
+//         element:<AdminRoute>
+//           <MakeAdmin></MakeAdmin>
+//         </AdminRoute>
+//       },{
+//         path:'assign-rider',
+//         element:<AdminRoute>
+//           <AssignParcel></AssignParcel>
+//         </AdminRoute>
+//       }
+
+//     ]
+//   }
+])
